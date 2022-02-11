97fc64253612ac45d9b38e6c45f64a345bb98004
James Shelbourne <jamesshbox@gmail.com>
2022-02-11 16:59:24 +0000

    TXMA-73 Initialise new Quarkus module using the AWS Lambda Archetype

---
+++ event-processor/src/test/java/uk/gov/di/audit/lambda/LambdaHandlerTestIT.java
@@ -0,0 +1,9 @@
+package uk.gov.di.audit.lambda;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class LambdaHandlerTestIT extends LambdaHandlerTest {
+
+    // Execute the same tests but in native mode.
+}
