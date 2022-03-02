4ec49220e78e1bb931e8280028639331812444e6
James Shelbourne <34451289+jashelb90@users.noreply.github.com>
2022-03-02 12:34:09 +0000

    Add Lambda Handler using Quarkus Archetype

---
+++ event-processor/src/main/java/uk/gov/di/audit/lambda/Person.java
@@ -0,0 +1,15 @@
+package uk.gov.di.audit.lambda;
+
+public class Person {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Person setName(String name) {
+        this.name = name;
+        return this;
+    }
+}
