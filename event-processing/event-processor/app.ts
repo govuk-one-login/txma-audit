3a185bc688f656e78becedf1fd87c9ebb870b0b7
James Shelbourne <james.shelbourne@digital.cabinet-office.gov.uk>
2022-03-17 14:18:40 +0000

    Remove comment

--- event-processing/event-processor/app.ts
+++ event-processing/event-processor/app.ts
@@ -19,7 +19,6 @@ export const handler = async (event: Event): Promise<string> => {
         throw new ValidationException('One or more event messages failed validation.', validationResponses);
     }
 
-    //Are we returning the records here or just the messages??? I assume just the messages
     return JSON.stringify(
         validationResponses.map((validationResponse: IValidationResponse) => {
             return validationResponse.message;
