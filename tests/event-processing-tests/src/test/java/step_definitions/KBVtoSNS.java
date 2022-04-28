4653f38c8f0e0cd380bd1dcf49a2501c3da41bd8
Denny Thampi <denny.thampi@digital.cabinet-office.gov.uk>
2022-04-28 16:31:46 +0100

    STep definitions updated

--- tests/event-processing-tests/src/test/java/step_definitions/KBVtoSNS.java
+++ tests/event-processing-tests/src/test/java/step_definitions/KBVtoSNS.java
@@ -5,5 +5,6 @@ import io.cucumber.java.en.Given;
 public class KBVtoSNS {
     @Given("a raw data event file <valid data from KBV> is available in SQS queue")
     public void aRawDataEventFileValidDataFromKBVIsAvailableInSQSQueue() {
+
     }
 }
--- tests/event-processing-tests/src/test/java/step_definitions/SPOTtoSNS.java
+++ tests/event-processing-tests/src/test/java/step_definitions/SPOTtoSNS.java
@@ -5,5 +5,6 @@ import io.cucumber.java.en.Given;
 public class SPOTtoSNS {
     @Given("a raw data event file <valid data from SPOT> is available in SQS queue")
     public void aRawDataEventFileValidDataFromSPOTIsAvailableInSQSQueue() {
+
     }
 }
