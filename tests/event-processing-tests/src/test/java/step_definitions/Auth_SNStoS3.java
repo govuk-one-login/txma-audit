5b31b4d47d8bc1c783041863fbd866118c0d3312
Denny Thampi <denny.thampi@digital.cabinet-office.gov.uk>
2022-04-28 14:35:40 +0100

    step definitions added

---
+++ tests/event-processing-tests/src/test/java/step_definitions/Auth_SNStoS3.java
@@ -0,0 +1,5 @@
+public class Auth_SNStoS3 {
+    @io.cucumber.java.en.Given("^the datafile <auth data file> is available in the SNS queue$")
+    public void theDatafileAuthDataFileIsAvailableInTheSNSQueue() {
+    }
+}
