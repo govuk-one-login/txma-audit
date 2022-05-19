afda8abc183372efa5f70341bb312a33f464bf7a
Jamie Maynard <29251905+j-maynard@users.noreply.github.com>
2022-05-19 17:02:20 +0100

    TMA-252 Update code to support input arrays
    It seems the events are coming as an array so we've updated the lambda
    to be able to process that as an array.

--- event-processing/obfuscation/app.ts
+++ event-processing/obfuscation/app.ts
@@ -9,20 +9,27 @@ import { ObfuscatedEvent } from './models/obfuscated-event';
 
 export const handler = async (event: FirehoseTransformationEvent): Promise<FirehoseTransformationResult> => {
     /* Process the list of records and transform them */
-    console.log("Recieved Event: "+ JSON.stringify(event));
     const output = event.records.map((record: FirehoseTransformationEventRecord) => {
         const plaintextData : string = Buffer.from(record.data, 'base64').toString('ascii');
-        console.log("Record Data: " + plaintextData);
-        const auditEvent : IAuditEvent = AuditEvent.fromJSONString(plaintextData);
-        let obfuscatedEvent = ObfuscatedEvent.fromAuditEvent(auditEvent);
-        console.log("Obfuscated Data: " + JSON.stringify(obfuscatedEvent));
+        let events : unknown[] = JSON.parse(plaintextData)
+        let obfuscatedEvents : ObfuscatedEvent[] = [];
+        let data : string;
+        if(events.length > 0) {
+            for(let i = 0; i < events.length; i++ ) {
+                const auditEvent : IAuditEvent = AuditEvent.fromJSONString(JSON.stringify(events[0]));
+                obfuscatedEvents.push(ObfuscatedEvent.fromAuditEvent(auditEvent));
+            }
+            data = Buffer.from(JSON.stringify(obfuscatedEvents)).toString('base64')
+        } else {
+            const auditEvent : IAuditEvent = AuditEvent.fromJSONString(plaintextData);
+            data = Buffer.from(JSON.stringify(ObfuscatedEvent.fromAuditEvent(auditEvent))).toString('base64')
+        }
         return {
             recordId: record.recordId,
             result: 'Ok',
-            data: Buffer.from(JSON.stringify(obfuscatedEvent)).toString('base64'),
+            data: data
         } as FirehoseTransformationResultRecord;
     });
     console.log(`Processing completed.  Successful records ${output.length}.`);
-    console.log("Output message: " + JSON.stringify(output));
     return { records: output };
 };
--- event-processing/obfuscation/tests/unit/app.test.ts
+++ event-processing/obfuscation/tests/unit/app.test.ts
@@ -39,7 +39,31 @@ describe('Unit test for app handler', function () {
         expect(result).toEqual(expectedResult);
     });
 
-    it('removes all additional fields', async () => {
+    it('removes all additional fields if its an array', async () => {
+        const expectedData: IObfuscatedEvent = {
+            timestamp: 1609462861,
+            timestamp_formatted: '2021-01-23T15:43:21.842',
+            event_name: 'AUTHENTICATION_ATTEMPT',
+        };
+
+        const data : string = Buffer.from(TestHelper.encodeAuditEventArray(expectedData)).toString('base64')
+        const expectedResult : FirehoseTransformationResult = {
+            records: [{
+                data: data,
+                recordId: "7041e12f-c772-41e4-a05f-8bf25cc6f4bb",
+                result: "Ok"
+            }]
+        }
+        
+        const firehoseEvent = TestHelper.createFirehoseEventWithEncodedMessage(TestHelper.encodeAuditEventArray(TestHelper.exampleMessage));
+        
+        const result = await handler(firehoseEvent);
+
+        expect(result).toEqual(expectedResult);
+        var resultData = Buffer.from(result.records[0].data, 'base64').toString('ascii');
+    });
+
+    it('removes all additional fields if its a single event', async () => {
         const expectedData: IObfuscatedEvent = {
             timestamp: 1609462861,
             timestamp_formatted: '2021-01-23T15:43:21.842',
@@ -61,6 +85,5 @@ describe('Unit test for app handler', function () {
 
         expect(result).toEqual(expectedResult);
         var resultData = Buffer.from(result.records[0].data, 'base64').toString('ascii');
-        console.log(JSON.stringify(resultData));
     });
 });
