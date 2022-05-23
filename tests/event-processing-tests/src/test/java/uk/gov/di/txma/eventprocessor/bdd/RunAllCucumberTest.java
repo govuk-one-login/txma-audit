d90aa185988a247a5c2dab7ff51434e884f738a8
Aidan23Smith <aidan.smith@digital.cabinet-office.gov.uk>
2022-05-23 14:42:53 +0100

    Delete accidental commit event-processing of Run Script

--- tests/event-processing-tests/src/test/java/uk/gov/di/txma/eventprocessor/bdd/RunAllCucumberTest.java
+++
@@ -1,7 +0,0 @@
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
-
-@Suite
-@SelectClasspathResource("features")
-public class RunAllCucumberTest {
-}
