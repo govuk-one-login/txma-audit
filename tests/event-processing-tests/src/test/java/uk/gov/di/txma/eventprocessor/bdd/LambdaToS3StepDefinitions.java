549e1b9475b67a591519a8ab7aafaf4fc160687b
Aidan23Smith <aidan.smith@digital.cabinet-office.gov.uk>
2022-06-07 17:26:06 +0100

    Update step definitions and feature
    Change scenario to an outline for all the service teams, and have a table of endpoints
    Remove other features
    
    Step definition:
    - Update some definitions to allow for a table of inputs to be looped through rather than calling it multiple times
    - Records the time when the lambda is invoked to provide a more accurate time for checking the S3 buckets
    - Takes finding a cloudwatch error to a separate class
    - Combines searching for a new data and checking it against the output file to allow a loop to work

--- tests/event-processing-tests/src/test/java/uk/gov/di/txma/eventprocessor/bdd/LambdaToS3StepDefinitions.java
+++ tests/event-processing-tests/src/test/java/uk/gov/di/txma/eventprocessor/bdd/LambdaToS3StepDefinitions.java
@@ -4,6 +4,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.datatable.DataTable;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import software.amazon.awssdk.core.SdkBytes;
@@ -27,23 +28,21 @@ import java.util.Objects;
 import java.util.Stack;
 import java.util.zip.GZIPInputStream;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class LambdaToS3StepDefinitions {
     Region region = Region.EU_WEST_2;
-    String output;
+    String output = null;
     String input;
-    Instant time = Instant.now();
+    Instant time;
     String requestid;
     String timestamp;
 
     /**
      * Checks that the input test data is present. And changes it to look like an SQS message
      *
-     * @param filename  The name of the file which act as the SQS input from the service teams' accounts
+     * @param filename      The name of the file which act as the SQS input from the service teams' accounts
      * @throws IOException
      */
     @Given("the SQS file {string} is available")
@@ -59,13 +58,19 @@ public class LambdaToS3StepDefinitions {
     /**
      * Checks the expected S3 output file is present
      *
-     * @param filename      The name of the file which should match the S3 data at the end of the test
+     * @param account      The name of the file which should match the S3 data at the end of the test
+     * @param fileNumber   The number of the file to check against
+     * @param endpoints     The endpoints we're checking against
      * @throws IOException
      */
-    @And("the output file {string} is available")
-    public void the_output_file_is_available(String filename) throws IOException{
-        Path filePath = Path.of(new File("src/test/resources/Test Data/" + filename).getAbsolutePath());
-        Files.readString(filePath);
+    @And("the {string} output file {string} is available")
+    public void the_output_file_is_available(String account, String fileNumber, DataTable endpoints) throws IOException{
+        // Loops through the possible endpoints
+        List<List<String>> data = endpoints.asLists(String.class);
+        for (List<String> endpoint : data) {
+            Path filePath = Path.of(new File("src/test/resources/Test Data/" + account + "/" + endpoint.get(0) + "_S3_" + fileNumber + ".json").getAbsolutePath());
+            Files.readString(filePath);
+        }
     }
 
     /**
@@ -101,156 +106,63 @@ public class LambdaToS3StepDefinitions {
             System.exit(1);
         }
 
+        // Records the time of when the lambda is sent to only check later objects in the S3
+        time = Instant.now();
     }
 
     /**
      * Ensures that an error does not appear in cloudwatch when the lambda was invoked
      *
-     * @param account               The service team account name for the corresponding lamdbda
-     * @throws InterruptedException
+     * @param account   The service team account name for the corresponding lambda
      */
     @Then("there shouldn't be an error message in the {string} lambda cloudwatch")
-    public void there_shouldnt_be_an_error_message_in_the_lambda_cloudwatch(String account) throws InterruptedException {
-
-        String logGroupName = "/aws/lambda/EventProcessorFunction-" + account;
-        // noEvent ensures we wait for cloudwatch to be updated
-        boolean noEvent = true;
-        Region region = Region.EU_WEST_2;
-
-        // Opens the cloudwatch client
-        try (CloudWatchLogsClient cloudWatchLogsClient = CloudWatchLogsClient.builder()
-                .region(region)
-                .build()){
-            while (noEvent){
-                // Gives the cloudwatch time to update
-                Thread.sleep(2000);
-
-                // Finds the latest messages in cloudwatch
-                DescribeLogStreamsRequest req = DescribeLogStreamsRequest.builder().logGroupName(logGroupName).orderBy("LastEventTime").descending(true).build();
-                DescribeLogStreamsResponse res2 = cloudWatchLogsClient.describeLogStreams(req);
-                String logStreamName = res2.logStreams().get(0).logStreamName();
-
-                // Get the messages from the latest batch of cloudwatch messages
-                GetLogEventsRequest getLogEventsRequest = GetLogEventsRequest.builder()
-                        .logGroupName(logGroupName)
-                        .logStreamName(logStreamName)
-                        .startFromHead(false)
-                        .build();
-
-                // Checks the logs don't contain an 'ERROR'
-                for (OutputLogEvent event : cloudWatchLogsClient.getLogEvents(getLogEventsRequest).events()){
-                    String message = event.message();
-                    if (message.contains(requestid)){
-                        assertThat(message, not(containsString("ERROR")));
-                        noEvent = false;
-                    }
-                }
-            }
-        } catch (CloudWatchException e) {
-            System.err.println(e.awsErrorDetails().errorMessage());
-            System.exit(1);
-        }
+    public void there_shouldnt_be_an_error_message_in_the_lambda_cloudwatch(String account) {
+        assertFalse(findCloudWatchError(account));
     }
 
     /**
-     * Checks the S3 bucket until a new message is found
+     * This searches the S3 bucket and checks that the new data contains the output file
      *
-     * @param endpoint              Which of the three S3 buckets we will check
-     * @throws InterruptedException
-     */
-    @And("the {string} s3 should have a new event data")
-    public void the_s3_should_have_a_new_event_data(String endpoint) throws InterruptedException {
-        String newkey = null;
-        String bucketName = "event-processing-build-"+endpoint+"-splunk-test";
-        // This ensures that the test will eventually fail, even if a new message is not found
-        int timer = 0;
-
-        Region region = Region.EU_WEST_2;
-
-        while (timer < 20 && newkey == null){
-            timer ++;
-            // Gives the message time to pass through Firehose
-            Thread.sleep(30000);
-
-            // Opens an S3 client
-            try (S3Client s3 = S3Client.builder()
-                    .region(region)
-                    .build()){
-
-                // Lists all objects
-                ListObjectsRequest listObjects = ListObjectsRequest
-                        .builder()
-                        .bucket(bucketName)
-                        .build();
-
-                // Finds the latest object
-                ListObjectsResponse res = s3.listObjects(listObjects);
-                List<S3Object> objects = res.contents();
-                S3Object latest = objects.get(objects.size() - 1);
-
-                // Checks it has been created past the time the test started and stored the corresponding key
-                if (latest.lastModified().compareTo(time)>0){
-                    newkey = latest.key();
-                }
-
-            } catch (S3Exception e) {
-                System.err.println(e.awsErrorDetails().errorMessage());
-                System.exit(1);
-            }
-        }
-
-        // Checks a new key was found
-        assertNotNull(newkey);
-
-        // Opens S3 client
-        try (S3Client s3 = S3Client.builder()
-                .region(region)
-                .build()){
-
-            // Gets the new object
-            GetObjectRequest objectRequest = GetObjectRequest
-                    .builder()
-                    .key(newkey)
-                    .bucket(bucketName)
-                    .build();
-
-            // Reads the new object
-            GZIPInputStream gzinpstr = new GZIPInputStream(s3.getObject(objectRequest));
-            InputStreamReader inpstr = new InputStreamReader(gzinpstr);
-            BufferedReader read = new BufferedReader(inpstr);
-            output = read.readLine();
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * Converts the test data to add the timestamp, and compares this to the new object
-     *
-     * @param filename      The name of the file which should match the S3 data at the end of the test
+     * @param account       Which account inputted the message
+     * @param fileNumber    Which file number we are checking
+     * @param endpoints     The endpoints we're checking against
      * @throws IOException
      */
-    @And("the event data should match with the {string} file")
-    public void the_event_data_should_match_with_the_file(String filename) throws IOException {
-        // Takes the input file, and adds a timestamp to the event_name
-        Path filePath = Path.of(new File("src/test/resources/Test Data/" + filename).getAbsolutePath());
-        String file = Files.readString(filePath);
-        JSONObject json = new JSONObject(file);
-        JSONObject expectedS3 = addTimestamp(json);
+    @And("the s3 below should have a new event matching the respective {string} output file {string}")
+    public void the_s3_below_should_have_a_new_event_matching_the_respective_output_files(String account, String fileNumber, DataTable endpoints) throws IOException {
 
-        // Splits the batched outputs into individual jsons
-        JSONArray array = separate(output);
+        // Loops through the possible outputs
+        List<List<String>> data = endpoints.asLists(String.class);
+        for (List<String> endpoint : data) {
+            output = null;
 
-        // Compares all individual jsons with our test data
-        boolean foundInS3 = false;
-        for (Object object: array){
-            if (Objects.equals(object.toString(), expectedS3.toString())){
-                foundInS3 = true;
+            // Checks for a new key
+            checkForNewKey(endpoint.get(0).toLowerCase());
+
+            assertNotNull(output);
+
+            // Takes the input file, and adds a timestamp to the event_name
+            Path filePath = Path.of(new File("src/test/resources/Test Data/" + account + "/" + endpoint.get(0) + "_S3_" + fileNumber + ".json").getAbsolutePath());
+            String file = Files.readString(filePath);
+            JSONObject json = new JSONObject(file);
+            JSONObject expectedS3 = addTimestamp(json);
+
+            // Splits the batched outputs into individual jsons
+            JSONArray array = separate(output);
+
+            // Compares all individual jsons with our test data
+            boolean foundInS3 = false;
+            for (Object object: array){
+                System.err.println(object.toString());
+                if (Objects.equals(object.toString(), expectedS3.toString())){
+                    foundInS3 = true;
+                    break;
+                }
             }
+
+            assertTrue(foundInS3);
         }
 
-        assertTrue(foundInS3);
     }
 
     /**
@@ -275,6 +187,73 @@ public class LambdaToS3StepDefinitions {
     }
 
     /**
+     * Searches the Cloudwatch Logs for the request id, and returns if an error was found
+     *
+     * @param account   The service team account name for the corresponding lambda
+     * @return          True if an error is found, false if not
+     */
+    private Boolean findCloudWatchError(String account){
+        String logGroupName = "/aws/lambda/EventProcessorFunction-" + account;
+        // noEvent ensures we wait for cloudwatch to be updated
+        boolean noEvent = true;
+        // errorFound tracks if we find an error or not
+        boolean errorFound = false;
+        // count will prevent an infinite loop if the event is not found
+        int count = 0;
+
+        // Opens the cloudwatch client
+        try (CloudWatchLogsClient cloudWatchLogsClient = CloudWatchLogsClient.builder()
+                .region(region)
+                .build()){
+            while (noEvent){
+                // Gives the cloudwatch time to update
+                Thread.sleep(4000);
+
+                // Finds all log streams in Cloudwatch
+                DescribeLogStreamsRequest req = DescribeLogStreamsRequest.builder().logGroupName(logGroupName).orderBy("LastEventTime").descending(true).limit(10).build();
+                DescribeLogStreamsResponse res2 = cloudWatchLogsClient.describeLogStreams(req);
+                List<LogStream> logStreams = res2.logStreams();
+
+                for (LogStream logStream : logStreams) {
+                    if (logStream.lastEventTimestamp() > time.getEpochSecond()) {
+                        String logStreamName = logStream.logStreamName();
+                        // Get the messages from the latest batch of cloudwatch messages
+                        GetLogEventsRequest getLogEventsRequest = GetLogEventsRequest.builder()
+                                .logGroupName(logGroupName)
+                                .logStreamName(logStreamName)
+                                .startFromHead(false)
+                                .build();
+
+                        // Checks the logs don't contain an 'ERROR'
+                        for (OutputLogEvent event : cloudWatchLogsClient.getLogEvents(getLogEventsRequest).events()) {
+                            String message = event.message();
+                            if (message.contains(requestid)) {
+                                if (message.contains("ERROR")) {
+                                    errorFound = true;
+                                }
+                                noEvent = false;
+                            }
+                        }
+                    }
+                }
+
+                count ++;
+                if (count == 30){
+                    System.err.println("No corresponding event found");
+                    System.exit(1);
+                }
+            }
+        } catch (CloudWatchException e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return errorFound;
+    }
+
+    /**
      * This adds the current timestamp to the event_name to ensure the message is unique
      *
      * @param json  This is the json which the event_name is being changed
@@ -288,6 +267,80 @@ public class LambdaToS3StepDefinitions {
             json.put("event_name", json.getString("event_name")+" "+timestamp);
         }
         return json;
+    }
+
+    /**
+     * This checks for a new key
+     *
+     * @param endpoint  What S3 bucket to look at
+     */
+    private void checkForNewKey(String endpoint){
+        String newkey = null;
+
+        String bucketName = "event-processing-build-"+endpoint+"-splunk-test";
+        // This ensures that the test will eventually fail, even if a new message is not found
+        int timer = 0;
+
+
+        while (timer < 5 && newkey == null){
+            timer ++;
+
+            // Opens an S3 client
+            try (S3Client s3 = S3Client.builder()
+                    .region(region)
+                    .build()){
+
+                // Lists all objects
+                ListObjectsRequest listObjects = ListObjectsRequest
+                        .builder()
+                        .bucket(bucketName)
+                        .build();
+
+                // Finds the latest object
+                ListObjectsResponse res = s3.listObjects(listObjects);
+                List<S3Object> objects = res.contents();
+                S3Object latest = objects.get(objects.size() - 1);
+
+                // Checks it has been created past the time the test started and stored the corresponding key
+                if (latest.lastModified().compareTo(time)>0){
+                    newkey = latest.key();
+                }
+                else {
+                    // Gives the message time to pass through Firehose
+                    Thread.sleep(30000);
+                }
+
+            } catch (S3Exception e) {
+                System.err.println(e.awsErrorDetails().errorMessage());
+                System.exit(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        if (newkey != null){
+            // Opens S3 client
+            try (S3Client s3 = S3Client.builder()
+                    .region(region)
+                    .build()){
+
+                // Gets the new object
+                GetObjectRequest objectRequest = GetObjectRequest
+                        .builder()
+                        .key(newkey)
+                        .bucket(bucketName)
+                        .build();
+
+                // Reads the new object
+                GZIPInputStream gzinpstr = new GZIPInputStream(s3.getObject(objectRequest));
+                InputStreamReader inpstr = new InputStreamReader(gzinpstr);
+                BufferedReader read = new BufferedReader(inpstr);
+                output = read.readLine();
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     /**
