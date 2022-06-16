6237b34b12727b5f23cb9fef7583adc5751e0b11
Aidan23Smith <aidan.smith@digital.cabinet-office.gov.uk>
2022-06-16 10:33:38 +0100

    Rename to include StepDefinitions in name

--- tests/audit-tests/src/test/java/uk/gov/di/txma/audit/bdd/FirehoseToS3StepDefinitions.java
+++ tests/audit-tests/src/test/java/uk/gov/di/txma/audit/bdd/FirehoseToS3StepDefinitions.java
@@ -40,7 +40,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-public class FirehoseToS3 {
+public class FirehoseToS3StepDefinitions {
 
     String output;
     SdkBytes input;
