0aadb56697baa944010504ff7ec93e33a07883f6
James Shelbourne <james.shelbourne@digital.cabinet-office.gov.uk>
2022-06-21 14:21:27 +0100

    TMA-354 Update the obfuscation-service.ts to handle obfuscating objects/arrays. This is to ensure all field names are retained and the structure is retained.

--- event-processing/obfuscation/services/obfuscation-service.ts
+++ event-processing/obfuscation/services/obfuscation-service.ts
@@ -6,21 +6,38 @@ export class ObfuscationService {
         if (auditEvent.user) {
             const user = auditEvent.user as any;
             for (const k in user) {
-                if (['transaction_id', 'email', 'phone'].indexOf(k) !== -1) user[k] = this.obfuscate(user[k], hmacKey);
+                if (['transaction_id', 'email', 'phone'].indexOf(k) !== -1)
+                    user[k] = this.obfuscateField(user[k], hmacKey);
             }
         }
 
         if (auditEvent.restricted) {
             const restricted = auditEvent.restricted as any;
             for (const k in restricted) {
-                restricted[k] = this.obfuscate(restricted[k], hmacKey);
+                if (typeof restricted[k] === 'object') {
+                    restricted[k] = this.obfuscateObject(restricted[k], hmacKey);
+                } else {
+                    restricted[k] = this.obfuscateField(restricted[k], hmacKey);
+                }
             }
         }
 
         return auditEvent;
     }
 
-    public static obfuscate(value: any, key: string): string {
+    public static obfuscateObject(value: any, key: string): any {
+        for (const k in value) {
+            if (typeof value[k] === 'object') {
+                value[k] = this.obfuscateObject(value[k], key);
+            } else {
+                value[k] = this.obfuscateField(value[k], key);
+            }
+        }
+
+        return value;
+    }
+
+    public static obfuscateField(value: any, key: string): string {
         if (typeof value != 'string') value = JSON.stringify(value);
 
         return crypto.createHmac('sha256', key).update(value).digest('hex');
