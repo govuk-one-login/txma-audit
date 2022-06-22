069637192b30f21a4a9c3a9cbacf14a6713dd955
James Shelbourne <james.shelbourne@digital.cabinet-office.gov.uk>
2022-06-22 16:20:23 +0100

    TMA-318 Update the run test scripts to run gradle tests and no longer create a dummy report.

--- tests/audit-tests/run-tests.sh
+++ tests/audit-tests/run-tests.sh
@@ -5,36 +5,7 @@ set -eu
 gradle -v
 
 echo "Current Environment $TEST_ENVIRONMENT"
-#./gradlew clean test -Dcucumber.filter.tags="@$TEST_ENVIRONMENT"
 
-cat <<EOF > "$TEST_REPORT_DIR/result.json"
-[
-  {
-    "uri": "test.sh",
-    "name": "Acceptance test",
-    "elements": [
-      {
-        "type": "scenario",
-        "name": "API Gateway request",
-        "line": 6,
-        "steps": [
-          {
-            "keyword": "Given ",
-            "name": "this step fails",
-            "line": 6,
-            "match": {
-              "location": "test.sh:4"
-            },
-            "result": {
-              "status": "passed",
-              "duration": 1
-            }
-          }
-        ]
-      }
-    ]
-  }
-]
-EOF
+./gradlew clean test --scan
 
 exit 0
