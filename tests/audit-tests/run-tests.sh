7f3c1a93e7ac2f4787c6f664a1db5d425c553ab2
James Shelbourne <james.shelbourne@digital.cabinet-office.gov.uk>
2022-06-28 16:20:50 +0100

    TMA-318 Enable Audit tests and tidy logging statement

--- tests/audit-tests/run-tests.sh
+++ tests/audit-tests/run-tests.sh
@@ -8,39 +8,9 @@ cd /
 
 echo "Environment: $TEST_ENVIRONMENT"
 
-echo "$PWD"
+echo "Current Working Directory: $PWD"
 
-#gradle -q test
-
-cat <<EOF > "$TEST_REPORT_ABSOLUTE_DIR/result.json"
-[
-  {
-    "uri": "test.sh",
-    "name": "Acceptance test",
-    "elements": [
-      {
-        "type": "scenario",
-        "name": "API Gateway request",
-        "line": 6,
-        "steps": [
-          {
-            "keyword": "Given ",
-            "name": "this step fails",
-            "line": 6,
-            "match": {
-              "location": "test.sh:4"
-            },
-            "result": {
-              "status": "passed",
-              "duration": 1
-            }
-          }
-        ]
-      }
-    ]
-  }
-]
-EOF
+gradle -q test
 
 echo "Successfully generated report"
 
