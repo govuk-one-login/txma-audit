01d12d6b2931263dd0c1d8faa005bb132f3d11a9
Jamie Maynard <29251905+j-maynard@users.noreply.github.com>
2022-06-29 15:00:18 +0100

    TMA-378 Updated clean method to handle arrays
    Updated clean method to handle arrays... Applied it to both the
    obfuscation lamnda and the event-processor.

--- event-processing/event-processor/services/sns-service.ts
+++ event-processing/event-processor/services/sns-service.ts
@@ -36,12 +36,18 @@ export class SnsService {
         return;
     }
 
-    static removeEmpty(obj : any) : unknown {
-        return Object.fromEntries(
-          Object.entries(obj)
-            .filter(([_, v]) => v != null)
-            .filter(([_, v]) => v != "")
-            .map(([k, v]) => [k, v === Object(v) ? this.removeEmpty(v) : v])
-        );
+    public static removeEmpty(obj : any) : unknown {
+        for (var propName in obj) {
+          if (obj[propName] === null || obj[propName] === undefined  || obj[propName] == "") {
+            delete obj[propName];
+          } else if(Array.isArray(obj[propName])) {
+            obj[propName].forEach((value : Object, index: string | number) => {
+                obj[propName][index] = this.removeEmpty(value);
+            });
+          } else if(typeof obj[propName] === 'object') {
+            obj[propName] = this.removeEmpty(obj[propName]);
+          }
+        }
+        return obj
     }
 }
