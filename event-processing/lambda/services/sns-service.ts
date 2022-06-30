2a2d3ea86604c38e12bdb6bff5bbd685a3d5f99c
James Shelbourne <james.shelbourne@digital.cabinet-office.gov.uk>
2022-06-30 09:04:37 +0100

    Fix lint errors after merge

--- event-processing/lambda/services/sns-service.ts
+++ event-processing/lambda/services/sns-service.ts
@@ -7,8 +7,8 @@ export class SnsService {
         console.log(`Topic ARN: ${topicArn}`);
 
         config.update({ region: process.env.AWS_REGION });
-        
-        let cleanMessage = this.removeEmpty(message);
+
+        const cleanMessage = this.removeEmpty(message);
 
         const params: PublishInput = {
             Message: JSON.stringify(cleanMessage),
@@ -36,18 +36,18 @@ export class SnsService {
         return;
     }
 
-    public static removeEmpty(obj : any) : unknown {
-        for (var propName in obj) {
-          if (obj[propName] === null || obj[propName] === undefined  || obj[propName] == "") {
-            delete obj[propName];
-          } else if(Array.isArray(obj[propName])) {
-            obj[propName].forEach((value : Object, index: string | number) => {
-                obj[propName][index] = this.removeEmpty(value);
-            });
-          } else if(typeof obj[propName] === 'object') {
-            obj[propName] = this.removeEmpty(obj[propName]);
-          }
+    public static removeEmpty(obj: any): unknown {
+        for (const propName in obj) {
+            if (obj[propName] === null || obj[propName] === undefined || obj[propName] == '') {
+                delete obj[propName];
+            } else if (Array.isArray(obj[propName])) {
+                obj[propName].forEach((value: unknown, index: string | number) => {
+                    obj[propName][index] = this.removeEmpty(value);
+                });
+            } else if (typeof obj[propName] === 'object') {
+                obj[propName] = this.removeEmpty(obj[propName]);
+            }
         }
-        return obj
+        return obj;
     }
 }
