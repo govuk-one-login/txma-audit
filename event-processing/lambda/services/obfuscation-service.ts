32ef324648f5de1494d34c0d76b0be51580543bf
James Shelbourne <james.shelbourne@digital.cabinet-office.gov.uk>
2022-07-08 08:36:31 +0100

    TMA-378 Remove redundant local removeEmpty method

--- event-processing/lambda/services/obfuscation-service.ts
+++ event-processing/lambda/services/obfuscation-service.ts
@@ -46,34 +46,4 @@ export class ObfuscationService {
         if (typeof value != 'string') value = JSON.stringify(value);
         return createHmac('sha256', key).update(value).digest('hex');
     }
-
-    public static removeEmpty(obj : any) : unknown {
-        for (var propName in obj) {
-          if (obj[propName] === null || obj[propName] === undefined  || obj[propName] == "") {
-            delete obj[propName];
-          } else if(Array.isArray(obj[propName])) {
-            obj[propName].forEach((value : Object, index: string | number) => {
-                obj[propName][index] = this.removeEmpty(value);
-            });
-          } else if(typeof obj[propName] === 'object') {
-            obj[propName] = this.removeEmpty(obj[propName]);
-          }
-        }
-        return obj
-    }
-
-    public static removeEmpty(obj : any) : unknown {
-        for (var propName in obj) {
-          if (obj[propName] === null || obj[propName] === undefined  || obj[propName] == "") {
-            delete obj[propName];
-          } else if(obj[propName] === typeof Array) {
-            obj[propName].forEach((value : Object, index: string | number) => {
-                obj[propName][index] = this.removeEmpty(value);
-            });
-          } else if(obj[propName] === typeof Object) {
-            obj[propName] = this.removeEmpty(obj[propName]);
-          }
-        }
-        return obj
-    }
 }
