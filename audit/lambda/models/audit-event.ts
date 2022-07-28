91171843c1bc49b485bf0968c843b7e853e03300
Aidan23Smith <aidan.smith@digital.cabinet-office.gov.uk>
2022-07-28 11:59:55 +0100

    Update lambda (and tests) to not use string as input variable
    Lambdas need to be called with objects, so an object structure is needed for the input

---
+++ audit/lambda/models/audit-event.ts
@@ -0,0 +1,23 @@
+export interface IAuditEvent {
+    event_id?: string;
+    client_id?: string;
+    timestamp: number;
+    timestamp_formatted?: string;
+    event_name: string;
+    component_id?: string;
+    user?: IAuditEventUserMessage | undefined;
+    platform?: unknown | undefined;
+    restricted?: unknown | undefined;
+    extensions?: unknown | undefined;
+}
+
+export interface IAuditEventUserMessage {
+    transaction_id?: string;
+    user_id?: string;
+    email?: string;
+    phone?: string;
+    ip_address?: string;
+    session_id?: string;
+    persistent_session_id?: string;
+    govuk_signin_journey_id?: string;
+}
