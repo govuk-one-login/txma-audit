3b1c93dfa42cdd5556e5296e1851203846c2f1f0
James Shelbourne <james.shelbourne@digital.cabinet-office.gov.uk>
2022-09-08 15:53:08 +0100

    TMA-206 Add logging to re-ingest-app.ts and update S3 policy to be Crud based policy to allow for deletion as part of re-ingest flow

--- event-processing/lambda/re-ingest-app.ts
+++ event-processing/lambda/re-ingest-app.ts
@@ -10,11 +10,13 @@ export const handler = async (event: S3Event): Promise<void> => {
     const bucket = event.Records[0].s3.bucket.name;
     const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
 
+    console.log(`Processing Starting....\n BucketName: ${bucket}\nBucketKey: ${key}`);
     //validate env vars
     validateEnvironmentVariables();
 
     const streamName = getStreamName(bucket);
     const maxIngest = Number(process.env.maxIngestion);
+    console.log(`Stream Details....\n StreamName: ${streamName}\nMaxIngest: ${maxIngest}`);
 
     try {
         const s3Object = await S3Service.getObject(bucket, key);
@@ -25,12 +27,15 @@ export const handler = async (event: S3Event): Promise<void> => {
             let destinationFH = 0;
             const s3ObjectLines = s3Object.split('\n');
 
+            console.log(`message count in object: ${s3ObjectLines.length}`);
             for (let lineIndex = 0; lineIndex < s3ObjectLines.length; lineIndex++) {
+                console.log(`Processing message: ${lineIndex + 1}`);
                 let destination = DestinationEnum.fireHose;
 
                 const currentLine = s3ObjectLines[lineIndex];
 
                 if (currentLine.length <= 0) {
+                    console.log(`Blank line, continuing....`);
                     continue;
                 }
 
@@ -43,19 +48,24 @@ export const handler = async (event: S3Event): Promise<void> => {
                     jsonData.reIngestCount = 1;
                 }
 
+                console.log(`Current ReIngest count: ${jsonData.reIngestCount}`);
+
                 if (jsonData.reIngestCount > maxIngest) {
+                    console.log(`Max ReIngest reached... archive to S3`);
                     deleteFromS3 += 1;
                     destination = DestinationEnum.s3;
                     s3Payload = s3Payload + JSON.stringify(jsonData) + '\n';
                 }
 
                 if (destination === DestinationEnum.fireHose) {
+                    console.log(`Push message to FireHose for ReProcessing...`);
                     const eventString = JSON.stringify(jsonData);
                     const encoder = new TextEncoder();
                     const messageBytes = encoder.encode(eventString);
                     recordBatch.push({ Data: messageBytes });
                     destinationFH++;
                     if (destinationFH > 499) {
+                        console.log(`Flush batch...`);
                         //flush max batch
                         await FirehoseService.putRecordsToFirehoseStream(streamName, recordBatch, 0, 20);
                         destinationFH = 0;
@@ -66,13 +76,14 @@ export const handler = async (event: S3Event): Promise<void> => {
 
             if (destinationFH > 0) {
                 try {
+                    console.log(`Push message to FireHose for ReProcessing...`);
                     await FirehoseService.putRecordsToFirehoseStream(streamName, recordBatch, 0, 20);
                 } catch (e) {
                     console.log(e);
                     throw e;
                 }
 
-                //only delete if sent or limit reached
+                console.log(`Delete object from S3... Bucket: ${bucket}, key: ${key}`);
                 await S3Service.deleteObject(bucket, key);
             }
 
@@ -82,9 +93,11 @@ export const handler = async (event: S3Event): Promise<void> => {
                         'One or more event records has been unsuccessfully delivered to splunk: ',
                 );
 
+                console.log(`Delete object from S3... Bucket: ${bucket}, key: ${key}`);
                 await S3Service.deleteObject(bucket, key);
             }
         } else {
+            console.log(`Delete object from S3... Bucket: ${bucket}, key: ${key}`);
             await S3Service.deleteObject(bucket, key);
         }
     } catch (error) {
