34591474047fe5cc82e781931c7c6b6dd0c697f3
Aidan23Smith <aidan.smith@digital.cabinet-office.gov.uk>
2022-09-16 12:00:46 +0100

    Create shared functions and clean code

--- tests/audit-tests/src/test/java/uk/gov/di/txma/audit/bdd/FirehoseToS3StepDefinitions.java
+++ tests/audit-tests/src/test/java/uk/gov/di/txma/audit/bdd/FirehoseToS3StepDefinitions.java
@@ -5,20 +5,13 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Instant;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.util.ArrayList;
 import java.util.Base64;
-import java.util.List;
-import java.util.zip.GZIPInputStream;
 
 import org.json.JSONObject;
 import software.amazon.awssdk.regions.Region;
@@ -27,12 +20,7 @@ import software.amazon.awssdk.services.lambda.LambdaClient;
 import software.amazon.awssdk.services.lambda.model.InvokeRequest;
 import software.amazon.awssdk.services.lambda.model.InvokeResponse;
 import software.amazon.awssdk.services.lambda.model.LambdaException;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
-import software.amazon.awssdk.services.s3.model.S3Exception;
-import software.amazon.awssdk.services.s3.model.S3Object;
+import uk.gov.di.txma.audit.utilities.S3SearchHelper;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -40,7 +28,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class FirehoseToS3StepDefinitions {
-    List<JSONObject> output = new ArrayList<>();
     String SNSInput;
     JSONObject expectedS3;
     Long timestamp = Instant.now().toEpochMilli();
@@ -50,7 +37,6 @@ public class FirehoseToS3StepDefinitions {
      * Checks that the input test data is present, and adds a timestamp to make it unique
      *
      * @param fileName  The name of the file which act as the SNS input from the event-processing account
-     * @throws IOException
      */
     @Given("the SNS file {string} is available")
     public void checkSNSInputFileIsAvailable(String fileName) throws IOException {
@@ -106,7 +92,7 @@ public class FirehoseToS3StepDefinitions {
      */
     @Then("the audit s3 should have a new event matching the output file")
     public void checkTheObjectInS3IsAsExpected() throws InterruptedException {
-        assertTrue(isFoundInS3(), "The message was not found in the S3 bucket.");
+        assertTrue(S3SearchHelper.isObjectFoundInS3(expectedS3), "The message was not found in the S3 bucket.");
     }
 
     /**
@@ -123,125 +109,8 @@ public class FirehoseToS3StepDefinitions {
         return messageAsJSON;
     }
 
-    /**
-     * Finds the latest 2 keys in the S3 bucket and saves the contents in the output variable
-     */
-    private void findLatestKeysFromAuditS3(){
-        String bucketName = "audit-" + System.getenv("TEST_ENVIRONMENT") + "-message-batch";
-
-        // The list of the latest two keys
-        List<String> keys = new ArrayList<>();
-
-        // Opens an S3 client
-        try (S3Client s3 = S3Client.builder()
-                .region(region)
-                .build()){
-
-            // This is used to get the current year, so that we can search the correct s3 files by prefix
-            ZonedDateTime currentDateTime = Instant.now().atZone(ZoneOffset.UTC);
-
-            // Lists 1000 objects
-            ListObjectsV2Request listObjects = ListObjectsV2Request
-                    .builder()
-                    .bucket(bucketName)
-                    .prefix("firehose/"+currentDateTime.getYear()+"/")
-                    .build();
-            ListObjectsV2Response res = s3.listObjectsV2(listObjects);
-            List<S3Object> objects = res.contents();
-
-            // If no objects were found, returns nothing
-            if (res.keyCount()==0){
-                return;
-            }
-
-            // Stores the most recent two keys
-            keys.add(objects.get(objects.size() - 1).key());
-            if (res.keyCount() > 1){
-                keys.add(objects.get(objects.size() - 2).key());
-            }
-
-            // If more than 1000 objects, cycles through the rest
-            while (res.isTruncated()){
-                // Gets the next batch
-                listObjects = ListObjectsV2Request
-                        .builder()
-                        .bucket(bucketName)
-                        .prefix("firehose/"+currentDateTime.getYear()+"/")
-                        .continuationToken(res.nextContinuationToken())
-                        .build();
-
-                // Stores the batch
-                res = s3.listObjectsV2(listObjects);
-                objects = res.contents();
-
-                // If there's more than one object, we store the most recent two keys
-                // If there is only one object, we keep the latest key from the previous batch,
-                // and store the most recent from this one
-                if (res.keyCount() > 1){
-                    keys.set(0, objects.get(objects.size() - 1).key());
-                    keys.set(1, objects.get(objects.size() - 2).key());
-                } else {
-                    keys.set(1, objects.get(0).key());
-                }
-            }
-
-            // Loops through the latest two keys
-            for (String key : keys){
-                // Gets the new object
-                GetObjectRequest objectRequest = GetObjectRequest
-                        .builder()
-                        .key(key)
-                        .bucket(bucketName)
-                        .build();
-
-                // Reads the new object
-                GZIPInputStream gzinpstr = new GZIPInputStream(s3.getObject(objectRequest));
-                InputStreamReader inpstr = new InputStreamReader(gzinpstr);
-                BufferedReader read = new BufferedReader(inpstr);
-                String line;
-                while ((line = read.readLine()) != null) {
-                    output.add(new JSONObject(line));
-                }
-            }
-
-        } catch (S3Exception e) {
-            System.err.println(e.awsErrorDetails().errorMessage());
-            System.exit(1);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     public String readJSONFile(String fileName) throws IOException {
         Path filePath = Path.of(new File("src/test/resources/Test Data/" + fileName + ".json").getAbsolutePath());
         return Files.readString(filePath);
-    }
-
-    public boolean isFoundInS3() throws InterruptedException {
-        // count will make sure it only searches for a finite time
-        int count = 0;
-
-        // Has a retry loop in case it finds the wrong key on the first try
-        // Count < 11 is enough time for it to be processed by the Firehose
-        while (count < 11) {
-            // Checks for latest key and saves the contents in the output variable
-            output = new ArrayList<>();
-            findLatestKeysFromAuditS3();
-
-            // If an object was found
-            if (output != null) {
-
-                // Compares all individual jsons with our test data
-                for (JSONObject object : output) {
-                    if (object.similar(expectedS3)) {
-                        return true;
-                    }
-                }
-            }
-
-            Thread.sleep(10000);
-            count ++;
-        }
-        return false;
     }
 }
