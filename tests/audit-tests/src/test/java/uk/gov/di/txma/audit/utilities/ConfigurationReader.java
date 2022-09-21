d566f800487cd1b9bb58cbafe1c3ccc2132b41c6
Aidan23Smith <aidan.smith@digital.cabinet-office.gov.uk>
2022-09-21 09:09:04 +0100

    Revert "Add URL from SSM"
    This reverts commit a1dad410e8aa8141e5914901b9940734765b75a7.

--- tests/audit-tests/src/test/java/uk/gov/di/txma/audit/utilities/ConfigurationReader.java
+++ tests/audit-tests/src/test/java/uk/gov/di/txma/audit/utilities/ConfigurationReader.java
@@ -1,42 +1,43 @@
 package uk.gov.di.txma.audit.utilities;
 
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.ssm.SsmClient;
-import software.amazon.awssdk.services.ssm.model.DescribeParametersRequest;
-import software.amazon.awssdk.services.ssm.model.DescribeParametersResponse;
-import software.amazon.awssdk.services.ssm.model.GetParameterRequest;
-import software.amazon.awssdk.services.ssm.model.GetParameterResponse;
-import software.amazon.awssdk.services.ssm.model.ParameterMetadata;
-import software.amazon.awssdk.services.ssm.model.SsmException;
-
+import java.io.FileInputStream;
 import java.util.Optional;
+import java.util.Properties;
 
 
 public class ConfigurationReader {
+
+    private static Properties properties;
+
+    static {
+
+        try {
+            String path = "configuration.properties";
+            FileInputStream input = new FileInputStream(path);
+            properties = new Properties();
+            properties.load(input);
+
+            input.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    public static String get(String keyName) {
+        return properties.getProperty(keyName);
+    }
 
     public static String getBrowser() {
         return Optional.ofNullable(System.getenv("BROWSER")).orElse("chrome-headless");
     }
 
-    public static String getUrl(String URLName){
-        try (SsmClient ssmClient = SsmClient.builder()
-                .region(Region.EU_WEST_2)
-                .build()){
-
-            GetParameterRequest parameterRequest = GetParameterRequest.builder()
-                    .name(URLName)
-                    .build();
-
-
-            GetParameterResponse parameterResponse = ssmClient.getParameter(parameterRequest);
-            return(parameterResponse.parameter().value());
-
-        } catch (SsmException e) {
-            System.out.println("message" + e.getMessage());
-            System.exit(1);
+    public static String getIPVCoreStubUrl() {
+        String orchestratorStubUrl = System.getenv("IPV_CORE_STUB_URL");
+        if (orchestratorStubUrl == null) {
+            throw new IllegalArgumentException("Environment variable IPV_CORE_STUB_URL is not set");
         }
-
-        return null;
+        return orchestratorStubUrl;
     }
 
 }
