144ca67845ee5d51c12da544c52a3c3275e2f070
Aidan23Smith <aidan.smith@digital.cabinet-office.gov.uk>
2022-09-21 10:22:05 +0100

    Add output for environment variables

--- tests/audit-tests/run-tests.sh
+++ tests/audit-tests/run-tests.sh
@@ -6,15 +6,13 @@ gradle -v
 
 cd /
 
-IPVCoreStubURL=$(aws cloudformation describe-stacks --region "eu-west-2" --stack-name "audit-config" --query "Stacks[0].Outputs[?OutputKey=='IPVCoreStubURL'].OutputValue" --output text)
-echo "IPVCoreStubURL: $IPVCoreStubURL"
-export IPVCoreStubURL=$IPVCoreStubURL
-
-OrchestrationStubURL=$(aws cloudformation describe-stacks --region "eu-west-2" --stack-name "audit-config" --query "Stacks[0].Outputs[?OutputKey=='OrchestrationStubURL'].OutputValue" --output text)
-echo "OrchestrationStubURL: $OrchestrationStubURL"
-export OrchestrationStubURL=$OrchestrationStubURL
-
 echo "Environment: $TEST_ENVIRONMENT"
+
+if [ "$TEST_ENVIRONMENT" = "build" ] || [ "$TEST_ENVIRONMENT" = "staging" ]
+then
+  echo "IPVCoreStubURL: $CFN_IPVCoreStubURL"
+  echo "OrchestrationStubURL: $CFN_OrchestrationStubURL"
+fi
 
 echo "Current Working Directory: $PWD"
 
