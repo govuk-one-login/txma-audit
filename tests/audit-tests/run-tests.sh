c89e54c36cb6f8ad599501b0096293bb20b32707
Aidan23Smith <aidan.smith@digital.cabinet-office.gov.uk>
2022-09-27 16:29:03 +0100

    Amend run-tests to check for local host

--- tests/audit-tests/run-tests.sh
+++ tests/audit-tests/run-tests.sh
@@ -4,7 +4,19 @@ set -eu
 
 gradle -v
 
-cd /
+# if we are running the test in docker compose, change to dev.Dockerfile WORKDIR otherwise preserve default behaviour
+echo "$DRIVER"
+if [ "$DRIVER" == "http://selenium-hub:4444/wd/hub" ]; then
+  echo "running in docker compose as the environment variable DRIVER is the selinium hubs service name as specified in docker-compose.dev.yml"
+  echo "preserving current working directory which is"
+  pwd
+else
+  echo "not in docker-compose"
+  cd /
+  docker compose -f "docker-compose.yml" up -d --build
+  jq --help
+  exit 1
+fi
 
 echo "Environment: $TEST_ENVIRONMENT"
 
