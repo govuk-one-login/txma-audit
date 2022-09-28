41cfaa4e0e4e7feb3f28db45d52225afb632894f
Zayd Patel <zayd.patel@digital.cabinet-office.gov.uk>
2022-09-28 11:07:57 +0100

    modify if statement to check if env var is actually set

--- tests/audit-tests/run-tests.sh
+++ tests/audit-tests/run-tests.sh
@@ -5,7 +5,7 @@ set -eu
 gradle -v
 
 # if we are running the test in docker compose, change to dev.Dockerfile WORKDIR otherwise preserve default behaviour
-if [ "$DRIVER" == "http://selenium-hub:4444/wd/hub" ]; then
+if [[ -z "${DRIVER}" ]] && [ "$DRIVER" == "http://selenium-hub:4444/wd/hub" ]; then
   echo "running in docker compose as the environment variable DRIVER is the selinium hubs service name as specified in docker-compose.dev.yml"
   echo "preserving current working directory which is"
   pwd
