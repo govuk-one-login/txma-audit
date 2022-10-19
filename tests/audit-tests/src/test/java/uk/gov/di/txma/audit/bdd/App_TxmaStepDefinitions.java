26fda8e3940113c7240d57ce412221b827cb8d5a
Denny Thampi <denny.thampi@digital.cabinet-office.gov.uk>
2022-10-19 11:51:29 +0100

    Updated the feature

--- tests/audit-tests/src/test/java/uk/gov/di/txma/audit/bdd/App_TxmaStepDefinitions.java
+++ tests/audit-tests/src/test/java/uk/gov/di/txma/audit/bdd/App_TxmaStepDefinitions.java
@@ -18,21 +18,21 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class App_TxmaStepDefinitions {
     String sessionId=null;
 
-    @Given("user is on APP API")
-    public void userIsOnAPPAPI() throws MalformedURLException {
+    @Given("user is on APP API stub")
+    public void userIsOnAPPAPIStub() throws MalformedURLException {
         Driver.get().get(ConfigurationReader.getAppURL());
         BrowserUtils.waitForPageToLoad(100);
 
     }
 
-    @When("user clicks on continue")
-    public void userClicksOnContinue() throws MalformedURLException {
+    @When("user clicks on continue on the Doc checking page")
+    public void userClicksOnContinueOnTheDocCheckingPage() throws MalformedURLException {
         new DocCheckingPage().Continue.click();
 
     }
 
-    @When("the user selects the session id")
-    public void theUserSelectsTheSessionId() throws MalformedURLException {
+    @When("the user selects the session id from the cookies")
+    public void theUserSelectsTheSessionIdFromTheCookies() throws MalformedURLException {
         Set<Cookie> cookies = Driver.get().manage().getCookies();
 
         if (cookies != null) {
@@ -48,8 +48,8 @@ public class App_TxmaStepDefinitions {
         Driver.get().quit();
     }
 
-    @Then("the audit S3 should have a new event with the user identifier subjectId")
-    public void theAuditSShouldHaveANewEventWithTheUserIdentifierSubjectId() throws InterruptedException {
+    @Then("the audit S3 should have a new event with the user identifier sessionId")
+    public void theAuditSShouldHaveANewEventWithTheUserIdentifierSessionId() throws InterruptedException {
 //        System.out.println(sessionId);
         assertTrue(S3SearchHelper.isStringFoundInS3(sessionId), "No event was found with user_id " + sessionId);
 
