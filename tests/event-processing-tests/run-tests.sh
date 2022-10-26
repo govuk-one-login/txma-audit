76fc75d78714d6e24bc949db1ae7cbdf15de26cd
Zayd Patel <zayd.patel@digital.cabinet-office.gov.uk>
2022-10-26 12:38:57 +0100

    update run tests to copy reports

--- tests/event-processing-tests/run-tests.sh
+++ tests/event-processing-tests/run-tests.sh
@@ -12,6 +12,9 @@ echo "Current Working Directory: $PWD"
 
 gradle -q test --info --stacktrace
 
+cp null/result.json report/result.json
+cp null/result.xml report/result.xml
+
 echo "Successfully generated report"
 
 exit 0
