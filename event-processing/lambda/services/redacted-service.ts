9a3e788fe6831c77e2eebb1711be73c146e3ba15
abdulkalam-govuk <abdul.kalam@digital.cabinet-office.gov.uk>
2022-11-11 18:54:47 +0000

    TMA-735 Failed to Consume Event Message to s3, review fix

--- event-processing/lambda/services/redacted-service.ts
+++ event-processing/lambda/services/redacted-service.ts
@@ -1,17 +1,6 @@
 import {IRedactedAuditEvent,  RedactedAuditEvent} from '../models/redacted-event';
 
-export class RedactedService {
-
-    static redactedEvent(auditEvent: IRedactedAuditEvent): IRedactedAuditEvent {
-        return new RedactedAuditEvent(
-            auditEvent.event_id,
-            auditEvent.event_name,
-            auditEvent.timestamp,
-            auditEvent.timestamp_formatted,
-            auditEvent.reIngestCount,
-            auditEvent.user,
-        );
-    }
+export class AccountsRedactedService {
 
     public static applyRedaction(snsMessage: string): IRedactedAuditEvent {
         const events: unknown[] = JSON.parse(snsMessage.toString());
