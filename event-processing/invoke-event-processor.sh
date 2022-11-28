05c9e6c8ec57f2fa23e97536c7ce8683520992f5
Zayd Patel <zayd.patel@digital.cabinet-office.gov.uk>
2022-11-28 10:05:49 +0000

    update path to event processor sample payload

--- event-processing/invoke-event-processor.sh
+++ event-processing/invoke-event-processor.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 AWS_PROFILE="event-processing-dev"
-PAYLOAD=$(cat invoke-event-processor-payload.json | base64  -)
+PAYLOAD=$(cat invoke-events/event.json | base64  -)
 FUNCTION_NAME="EventProcessorFunction-TxMATwoAudit"
 
 aws lambda invoke --function-name "$FUNCTION_NAME" --invocation-type Event --payload "$PAYLOAD" outfile.txt --profile $AWS_PROFILE
