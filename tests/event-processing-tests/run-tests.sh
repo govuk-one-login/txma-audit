a9634ff2ca0d32acdb81f298ee73069d7cfd9454
Denny Thampi <denny.thampi@digital.cabinet-office.gov.uk>
2022-12-06 16:48:57 +0000

    test updated to include the sqs url in ssm

--- tests/event-processing-tests/run-tests.sh
+++ tests/event-processing-tests/run-tests.sh
@@ -16,7 +16,7 @@ echo "Environment: $TEST_ENVIRONMENT"
 
 echo "Current Working Directory: $PWD"
 
- gradle -q test --info --stacktrace
+# gradle -q test --info --stacktrace
 
 cp null/result.json $TEST_ENVIRONMENT/result.json
 cp null/result.xml $TEST_ENVIRONMENT/result.xml
