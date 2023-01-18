f680ccd6bf216b70a6758b3938b1e2de606d429a
Tom Pickering <tom.pickering@digital.cabinet-office.gov.uk>
2023-01-18 16:49:50 +0000

    TT2-233 amend filter for cleansing user object

--- event-processing/lambda/tests/unit/services/cleansing-service.test.ts
+++ event-processing/lambda/tests/unit/services/cleansing-service.test.ts
@@ -3,27 +3,33 @@ import { CleansingService } from '../../../services/cleansing-service';
 import { IEnrichedAuditEvent } from '../../../models/enriched-audit-event';
 import { ICleansedEvent } from '../../../models/cleansed-event';
 
+const inputMessage: IEnrichedAuditEvent = {
+    timestamp: 1609462861,
+    timestamp_formatted: '2021-01-23T15:43:21.842',
+    event_name: 'AUTHENTICATION_ATTEMPT',
+    event_id: '123456789',
+    component_id: '1234',
+    client_id: 'An Example Client',
+    reIngestCount: 0,
+};
+
 describe('Unit test for cleansing-service', function () {
-    it('returns a cleansed event', async () => {
-        const inputMessage: IEnrichedAuditEvent = {
-            timestamp: 1609462861,
-            timestamp_formatted: '2021-01-23T15:43:21.842',
-            event_name: 'AUTHENTICATION_ATTEMPT',
-            event_id: '123456789',
-            component_id: '1234',
-            client_id: 'An Example Client',
-            user: {
-                transaction_id: 'a52f6f87',
-                user_id: 'some_user_id',
-                email: 'foo@bar.com',
-                phone: '07711223344',
-                ip_address: '100.100.100.100',
-                session_id: 'c222c1ec',
-                persistent_session_id: 'some session id',
-                govuk_signin_journey_id: '43143-233Ds-2823-283-dj299j1',
-                device_id: 'some known device',
-            },
-            reIngestCount: 0,
+
+    afterEach(() => {
+        inputMessage.user = {};
+    });
+
+    it('returns a cleansed event with all required user information', async () => {
+        inputMessage.user = {
+            transaction_id: 'a52f6f87',
+            user_id: 'some_user_id',
+            email: 'foo@bar.com',
+            phone: '07711223344',
+            ip_address: '100.100.100.100',
+            session_id: 'c222c1ec',
+            persistent_session_id: 'some session id',
+            govuk_signin_journey_id: '43143-233Ds-2823-283-dj299j1',
+            device_id: 'some known device',
         };
 
         const expectedMessage: ICleansedEvent = {
@@ -37,6 +43,86 @@ describe('Unit test for cleansing-service', function () {
                 user_id: 'some_user_id',
                 govuk_signin_journey_id: '43143-233Ds-2823-283-dj299j1',
             },
+            reIngestCount: 0,
+        };
+
+        expect(CleansingService.cleanseEvent(inputMessage)).toEqual(expectedMessage);
+    });
+
+    it('returns a cleansed event with only user_id', async () => {
+        inputMessage.user = {
+            transaction_id: 'a52f6f87',
+            user_id: 'some_user_id',
+            email: 'foo@bar.com',
+            phone: '07711223344',
+            ip_address: '100.100.100.100',
+            session_id: 'c222c1ec',
+            persistent_session_id: 'some session id',
+            device_id: 'some known device',
+        };
+
+        const expectedMessage: ICleansedEvent = {
+            timestamp: 1609462861,
+            timestamp_formatted: '2021-01-23T15:43:21.842',
+            event_name: 'AUTHENTICATION_ATTEMPT',
+            event_id: '123456789',
+            component_id: '1234',
+            client_id: 'An Example Client',
+            user: {
+                user_id: 'some_user_id',
+            },
+            reIngestCount: 0,
+        };
+
+        expect(CleansingService.cleanseEvent(inputMessage)).toEqual(expectedMessage);
+    });
+
+    it('returns a cleansed event with only govuk_signin_journey_id', async () => {
+        inputMessage.user = {
+            transaction_id: 'a52f6f87',
+            email: 'foo@bar.com',
+            phone: '07711223344',
+            ip_address: '100.100.100.100',
+            session_id: 'c222c1ec',
+            persistent_session_id: 'some session id',
+            govuk_signin_journey_id: '43143-233Ds-2823-283-dj299j1',
+            device_id: 'some known device',
+        };
+
+        const expectedMessage: ICleansedEvent = {
+            timestamp: 1609462861,
+            timestamp_formatted: '2021-01-23T15:43:21.842',
+            event_name: 'AUTHENTICATION_ATTEMPT',
+            event_id: '123456789',
+            component_id: '1234',
+            client_id: 'An Example Client',
+            user: {
+                govuk_signin_journey_id: '43143-233Ds-2823-283-dj299j1',
+            },
+            reIngestCount: 0,
+        };
+
+        expect(CleansingService.cleanseEvent(inputMessage)).toEqual(expectedMessage);
+    });
+
+    it('returns a cleansed event with no user information if required fields not present', async () => {
+        inputMessage.user = {
+            transaction_id: 'a52f6f87',
+            email: 'foo@bar.com',
+            phone: '07711223344',
+            ip_address: '100.100.100.100',
+            session_id: 'c222c1ec',
+            persistent_session_id: 'some session id',
+            device_id: 'some known device',
+        };
+
+        const expectedMessage: ICleansedEvent = {
+            timestamp: 1609462861,
+            timestamp_formatted: '2021-01-23T15:43:21.842',
+            event_name: 'AUTHENTICATION_ATTEMPT',
+            event_id: '123456789',
+            component_id: '1234',
+            client_id: 'An Example Client',
             reIngestCount: 0,
         };
 
