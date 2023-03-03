867e6659b3660ad5ad8d0076b21f9d8a43f84cbc
andrewmccafferty <andrew.mccafferty@digital.cabinet-office.gov.uk>
2023-03-03 16:46:01 +0000

    Inline log level code

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -2,17 +2,9 @@ import { Logger } from '@aws-lambda-powertools/logger'
 import { LogLevel } from '@aws-lambda-powertools/logger/lib/types/Log'
 import { Context } from 'aws-lambda'
 
-const getLogLevel = (): LogLevel => {
-  if (process.env.LOG_LEVEL) {
-    return process.env.LOG_LEVEL as LogLevel
-  }
-
-  return 'DEBUG'
-}
-
 const loggerInstance = new Logger({
   serviceName: process.env.AWS_LAMBDA_FUNCTION_NAME,
-  logLevel: getLogLevel()
+  logLevel: (process.env.LOG_LEVEL as LogLevel) || 'DEBUG'
 })
 
 export const initialiseLogger = (context: Context) => {
