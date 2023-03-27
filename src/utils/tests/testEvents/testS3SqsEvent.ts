ef6e909e8ced6c8165079f7e9ec13dd961b57245
andrewmccafferty <andrew.mccafferty@digital.cabinet-office.gov.uk>
2023-03-27 15:59:22 +0100

    Refactor the encryption code into a method that can be used from multiple lambdas

--- src/utils/tests/testEvents/testS3SqsEvent.ts
+++ src/utils/tests/testEvents/testS3SqsEvent.ts
@@ -1,16 +1,9 @@
-import {
-  TEST_TEMPORARY_BUCKET_NAME,
-  TEST_S3_OBJECT_KEY,
-  TEST_WRONG_S3_BUCKET,
-  TEST_AUDIT_BUCKET_NAME
-} from '../testConstants'
-
-export const testTemporaryS3SqsEvent = {
+export const testS3SqsEvent = (bucketName: string, objectKey: string) => ({
   Records: [
     {
       messageId: '',
       receiptHandle: '',
-      body: `{"Records":[{"eventVersion":"","eventSource":"","awsRegion":"","eventTime":"","eventName":"","userIdentity":{"principalId":""},"requestParameters":{"sourceIPAddress":""},"responseElements":{"x-amz-request-id":"","x-amz-id-2":""},"s3":{"s3SchemaVersion":"","configurationId":"","bucket":{"name":"${TEST_TEMPORARY_BUCKET_NAME}","ownerIdentity":{"principalId":""},"arn":""},"object":{"key":"${TEST_S3_OBJECT_KEY}","size":1,"eTag":"","versionId":"","sequencer":""}}}]}`,
+      body: `{"Records":[{"eventVersion":"","eventSource":"","awsRegion":"","eventTime":"","eventName":"","userIdentity":{"principalId":""},"requestParameters":{"sourceIPAddress":""},"responseElements":{"x-amz-request-id":"","x-amz-id-2":""},"s3":{"s3SchemaVersion":"","configurationId":"","bucket":{"name":"${bucketName}","ownerIdentity":{"principalId":""},"arn":""},"object":{"key":"${objectKey}","size":1,"eTag":"","versionId":"","sequencer":""}}}]}`,
       attributes: {
         ApproximateReceiveCount: '1',
         SentTimestamp: '',
@@ -24,50 +17,7 @@ export const testTemporaryS3SqsEvent = {
       awsRegion: ''
     }
   ]
-}
-
-export const testAuditS3SqsEvent = {
-  Records: [
-    {
-      messageId: '',
-      receiptHandle: '',
-      body: `{"Records":[{"eventVersion":"","eventSource":"","awsRegion":"","eventTime":"","eventName":"","userIdentity":{"principalId":""},"requestParameters":{"sourceIPAddress":""},"responseElements":{"x-amz-request-id":"","x-amz-id-2":""},"s3":{"s3SchemaVersion":"","configurationId":"","bucket":{"name":"${TEST_AUDIT_BUCKET_NAME}","ownerIdentity":{"principalId":""},"arn":""},"object":{"key":"${TEST_S3_OBJECT_KEY}","size":1,"eTag":"","versionId":"","sequencer":""}}}]}`,
-      attributes: {
-        ApproximateReceiveCount: '1',
-        SentTimestamp: '',
-        SenderId: '',
-        ApproximateFirstReceiveTimestamp: ''
-      },
-      messageAttributes: {},
-      md5OfBody: '',
-      eventSource: '',
-      eventSourceARN: '',
-      awsRegion: ''
-    }
-  ]
-}
-
-export const wrongBucketTestS3SqsEvent = {
-  Records: [
-    {
-      messageId: '',
-      receiptHandle: '',
-      body: `{"Records":[{"eventVersion":"","eventSource":"","awsRegion":"","eventTime":"","eventName":"","userIdentity":{"principalId":""},"requestParameters":{"sourceIPAddress":""},"responseElements":{"x-amz-request-id":"","x-amz-id-2":""},"s3":{"s3SchemaVersion":"","configurationId":"","bucket":{"name":"${TEST_WRONG_S3_BUCKET}","ownerIdentity":{"principalId":""},"arn":""},"object":{"key":"${TEST_S3_OBJECT_KEY}","size":1,"eTag":"","versionId":"","sequencer":""}}}]}`,
-
-      attributes: {
-        ApproximateReceiveCount: '1',
-        SentTimestamp: '',
-        SenderId: '',
-        ApproximateFirstReceiveTimestamp: ''
-      },
-      messageAttributes: {},
-      md5OfBody: '',
-      eventSource: '',
-      eventSourceARN: '',
-      awsRegion: ''
-    }
-  ]
-}
+})
 
 export const testS3TestEvent = {
   Records: [
