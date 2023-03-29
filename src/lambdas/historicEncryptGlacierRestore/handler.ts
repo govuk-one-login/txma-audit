095b208c819edb6d8c0d085f4de5c7d3bfc23d44
andrewmccafferty <andrew.mccafferty@digital.cabinet-office.gov.uk>
2023-03-29 14:28:42 +0100

    Final changes to allow encryption of files as they're restored

--- src/lambdas/historicEncryptGlacierRestore/handler.ts
+++ src/lambdas/historicEncryptGlacierRestore/handler.ts
@@ -1,11 +1,26 @@
-import { Context, S3ObjectRestoreCompletedNotificationEvent } from 'aws-lambda'
-import { initialiseLogger } from '../../services/logger'
+import { Context, S3Event } from 'aws-lambda'
+import { initialiseLogger, logger } from '../../services/logger'
 import { encryptAuditData } from '../../sharedServices/encryptAuditData'
+import { permanentBucketFileExists } from './permanentBucketFileExists'
 
-export const handler = async (
-  event: S3ObjectRestoreCompletedNotificationEvent,
-  context: Context
-) => {
+export const handler = async (event: S3Event, context: Context) => {
   initialiseLogger(context)
-  await encryptAuditData(event.detail.bucket.name, event.detail.object.key)
+  if (!event.Records || event.Records.length !== 1) {
+    logger.error(
+      `Event record length was '${event.Records.length}' not 1, or no Records property found, cannot continue`
+    )
+    return
+  }
+
+  const s3Data = event.Records[0].s3
+  const objectKey = s3Data.object.key
+  if (await permanentBucketFileExists(objectKey)) {
+    logger.info(
+      'File to encrypt already found in permanent bucket, so no new encryption will happen',
+      { objectKey }
+    )
+    return
+  }
+
+  await encryptAuditData(s3Data.bucket.name, objectKey)
 }
