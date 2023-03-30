9902cb1a453887bd6bef60ac2b9483f08223a053
andrewmccafferty <andrew.mccafferty@digital.cabinet-office.gov.uk>
2023-03-30 12:00:39 +0100

    Add env var setup file

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,8 @@ import type { Config } from '@jest/types'
 const config: Config.InitialOptions = {
   coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
   preset: 'ts-jest',
-  verbose: true
+  verbose: true,
+  setupFiles: ['<rootDir>/src/utils/tests/setup/testEnvVars.ts']
 }
 
 export default config
