547204f6050a8179f7a703873f756ee9d5c9b102
Andy Langridge <andy.langridge@digital.cabinet-office.gov.uk>
2023-06-14 18:51:40 +0100

    Add duplicate resources with names that won't clash

--- src/lambdas/historicEncryptGlacierRestore/permanentBucketFileExists.ts
+++
@@ -1,6 +0,0 @@
-import { s3FileExists } from '../../services/s3/s3FileExists'
-import { getEnv } from '../../utils/helpers'
-
-export const permanentBucketFileExists = (s3Key: string): Promise<boolean> => {
-  return s3FileExists(getEnv('PERMANENT_BUCKET_NAME'), s3Key)
-}
