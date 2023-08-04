aef2f1b10e65e799bf72e57dd084e2260b703b64
Zayd Patel <zayd.patel@digital.cabinet-office.gov.uk>
2023-08-04 15:30:02 +0100

    WIP inital lambda

---
+++ src/lambdas/RedriveSNSDQLEvents/handler.ts
@@ -0,0 +1,23 @@
+import { SQSEvent, Context, SQSBatchResponse } from 'aws-lambda'
+import { tryParseJSON } from '../../utils/helpers/tryParseJson'
+import { initialiseLogger, logger } from '../../sharedServices/logger'
+export const handler = async (
+  event: SQSEvent,
+  context: Context
+): Promise<SQSBatchResponse> => {
+  initialiseLogger(context)
+
+  const results = event.Records.map((sqsRecord) => {
+    const parsedRecord = tryParseJSON(sqsRecord.body)
+    logger.info('parsedRecord', parsedRecord)
+    return sqsRecord.messageId
+  })
+
+  logger.info('sqs message ids', {
+    messageId: results
+  })
+
+  return {
+    batchItemFailures: []
+  }
+}
