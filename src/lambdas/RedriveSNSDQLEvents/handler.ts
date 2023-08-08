da54e90bac3f85f1a7f8179c382c8025fd729157
Zayd Patel <zayd.patel@digital.cabinet-office.gov.uk>
2023-08-08 10:07:03 +0100

    WIP write to firehose

--- src/lambdas/RedriveSNSDQLEvents/handler.ts
+++ src/lambdas/RedriveSNSDQLEvents/handler.ts
@@ -1,23 +1,82 @@
-import { SQSEvent, Context, SQSBatchResponse } from 'aws-lambda'
+import {
+  SQSEvent,
+  Context,
+  SQSBatchResponse,
+  SQSBatchItemFailure
+} from 'aws-lambda'
 import { tryParseJSON } from '../../utils/helpers/tryParseJson'
 import { initialiseLogger, logger } from '../../sharedServices/logger'
+import { AuditEvent } from '../../types/auditEvent'
+import { auditEventsToFirehoseRecords } from '../../utils/helpers/firehose/auditEventsToFirehoseRecords'
+import { firehosePutRecordBatch } from '../../sharedServices/firehose/firehosePutRecordBatch'
+import { getEnv } from '../../utils/helpers/getEnv'
+
+export type ProcessingResult = {
+  sqsMessageId: string
+  failed: boolean
+  failureReason?: string
+  auditEvent?: AuditEvent
+}
+
 export const handler = async (
   event: SQSEvent,
   context: Context
 ): Promise<SQSBatchResponse> => {
   initialiseLogger(context)
 
-  const results = event.Records.map((sqsRecord) => {
-    const parsedRecord = tryParseJSON(sqsRecord.body)
-    logger.info('parsedRecord', { parsedRecord })
-    return sqsRecord.messageId
+  const results: ProcessingResult[] = event.Records.map((sqsRecord) => {
+    const parsedRecord = tryParseJSON(sqsRecord.body) as AuditEvent
+
+    if (typeof parsedRecord.event_id === 'undefined') {
+      return {
+        sqsMessageId: sqsRecord.messageId,
+        failed: true,
+        name: 'ParsingJSONError'
+      }
+    }
+
+    return {
+      sqsMessageId: sqsRecord.messageId,
+      failed: false,
+      auditEvent: parsedRecord
+    }
   })
 
-  logger.info('sqs message ids', {
-    messageId: results
+  const successfullyParsedRecords = results.filter(
+    (result) => result.failed === true
+  )
+  const unsuccessfullyParsedRecords = results.filter(
+    (result) => result.failed === false
+  )
+
+  const unsuccessfullyParsedRecordsSQSMessageId =
+    SQSBatchItemFailureFromProcessingResultArray(unsuccessfullyParsedRecords)
+
+  const auditEventArray = successfullyParsedRecords.map((element) => {
+    return element.auditEvent as AuditEvent
   })
+
+  const firehoseRecords = auditEventsToFirehoseRecords(auditEventArray)
+
+  try {
+    const result = await firehosePutRecordBatch(
+      getEnv('FIREHOSE_DELIVERY_STREAM_NAME'),
+      firehoseRecords
+    )
+    logger.debug('result', { result })
+  } catch (error) {
+    logger.error('failed to publish to firehose', error as Error)
+  }
 
   return {
-    batchItemFailures: []
+    batchItemFailures: unsuccessfullyParsedRecordsSQSMessageId
   }
+}
+
+const SQSBatchItemFailureFromProcessingResultArray = (
+  processingResultArray: ProcessingResult[]
+): SQSBatchItemFailure[] => {
+  return processingResultArray.map((element) => {
+    return { itemIdentifier: element.sqsMessageId }
+  })
 }
