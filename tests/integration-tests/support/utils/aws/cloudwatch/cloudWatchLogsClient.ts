633da33b2757797edcc8eae408df2b61b9dd0d07
andrewmccafferty <andrew.mccafferty@digital.cabinet-office.gov.uk>
2023-08-11 16:45:05 +0100

    Remove redundant code

--- tests/integration-tests/support/utils/aws/cloudwatch/cloudWatchLogsClient.ts
+++
@@ -1,6 +0,0 @@
-import { CloudWatchLogsClient } from '@aws-sdk/client-cloudwatch-logs'
-import { getEnv } from '../../getEnv'
-
-export const cloudWatchLogsClient = new CloudWatchLogsClient({
-  region: getEnv('AWS_REGION')
-})
