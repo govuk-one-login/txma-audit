d1dee397ea6e4faa6c58c8b19cc8aab1123aa55e
andrewmccafferty <andrew.mccafferty@digital.cabinet-office.gov.uk>
2023-08-11 16:40:21 +0100

    Remove redundant file

--- tests/integration-tests/constants/cloudWatchLogFilters.ts
+++
@@ -1,9 +0,0 @@
-export const cloudwatchLogFilters: CloudwatchLogFiltersConstants = {
-  eventPublished: 'Successfully processed',
-  firehouseSuccess: 'Successfully processed'
-}
-
-type CloudwatchLogFiltersConstants = {
-  readonly eventPublished: string
-  readonly firehouseSuccess: string
-}
