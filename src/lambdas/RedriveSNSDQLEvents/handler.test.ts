3d2fd453548755f9b8c13532740f33bc586530ed
Zayd Patel <zayd.patel@digital.cabinet-office.gov.uk>
2023-08-11 13:04:40 +0100

    write unit tests for `parseSQSEvent()`

--- src/lambdas/RedriveSNSDQLEvents/handler.test.ts
+++ src/lambdas/RedriveSNSDQLEvents/handler.test.ts
@@ -1,6 +1,8 @@
 import { SQSEvent } from 'aws-lambda'
+import { AuditEvent } from '../../types/auditEvent'
 import {
   generateEventIdLogMessageFromProcessingResult,
+  parseSQSEvent,
   ProcessingResult,
   SQSBatchItemFailureFromProcessingResultArray
 } from './handler'
@@ -13,7 +15,12 @@ export const baseProcessingResults: ProcessingResult[] = [
     auditEvent: {
       event_id: '987654321',
       event_name: 'EVENT_ONE',
-      timestamp: 1691673691
+      timestamp: 1691673691,
+      txma: {
+        failedSNSPublish: {
+          audit: true
+        }
+      }
     }
   },
   {
@@ -23,7 +30,12 @@ export const baseProcessingResults: ProcessingResult[] = [
     auditEvent: {
       event_id: '098765432',
       event_name: 'EVENT_TWO',
-      timestamp: 1691673691
+      timestamp: 1691673691,
+      txma: {
+        failedSNSPublish: {
+          audit: true
+        }
+      }
     }
   },
   {
@@ -33,10 +45,26 @@ export const baseProcessingResults: ProcessingResult[] = [
     auditEvent: {
       event_id: '109876543',
       event_name: 'EVENT_THREE',
-      timestamp: 1691673691
+      timestamp: 1691673691,
+      txma: {
+        failedSNSPublish: {
+          audit: true
+        }
+      }
     }
   }
 ]
+
+const baseSQSEvent = {
+  Records: baseProcessingResults.map((result) => {
+    const event = JSON.stringify(result.auditEvent)
+    const response = {
+      messageId: result.sqsMessageId,
+      body: event
+    }
+    return response
+  })
+} as SQSEvent
 
 describe('testing helper functions', () => {
   it('test SQSBatchItemFailureFromProcessingResultArray() to support partial failure response ', async () => {
@@ -72,6 +100,57 @@ describe('testing helper functions', () => {
       ParsingJSONError: testInput[1].map((result) => {
         return result.auditEvent?.event_id
       })
+    }
+    expect(result).toStrictEqual(expectedResult)
+  })
+
+  it('test parseSQSEvent(). All records successfully parsed', async () => {
+    const result = parseSQSEvent(baseSQSEvent)
+    const expectedResult = {
+      successfullyParsedRecords: baseProcessingResults.slice(),
+      unsuccessfullyParsedRecords: []
+    }
+    expect(result).toStrictEqual(expectedResult)
+  })
+
+  it('test parseSQSEvent(). partial records successfully parsed', async () => {
+    const partialyInvalidSQSEvent = {
+      Records: baseProcessingResults.map((result, index) => {
+        if (index === 0) {
+          const event = result.auditEvent as AuditEvent
+          delete event.event_id
+          const tweakedEvent = JSON.stringify(event)
+          const response = {
+            messageId: result.sqsMessageId,
+            body: tweakedEvent
+          }
+          return response
+        } else {
+          const event = JSON.stringify(result.auditEvent)
+          const response = {
+            messageId: result.sqsMessageId,
+            body: event
+          }
+          return response
+        }
+      })
+    } as SQSEvent
+
+    const result = parseSQSEvent(partialyInvalidSQSEvent)
+    const expectedResult = {
+      successfullyParsedRecords: baseProcessingResults.slice(1),
+      unsuccessfullyParsedRecords: baseProcessingResults
+        .slice(0, 1)
+        .map((processingResult) => {
+          const tweakedProcessingResult = { ...processingResult }
+          delete tweakedProcessingResult.auditEvent
+
+          return {
+            ...tweakedProcessingResult,
+            failed: true,
+            statusReason: 'ParsingJSONError'
+          }
+        })
     }
     expect(result).toStrictEqual(expectedResult)
   })
