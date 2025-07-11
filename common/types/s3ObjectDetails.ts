0467ab6820749caeaf1aff60078be0d9f4737689
Umar Hansa <umar.hansa@digital.cabinet-office.gov.uk>
2025-07-11 12:55:45 +0100

    Address code review

--- common/types/s3ObjectDetails.ts
+++ common/types/s3ObjectDetails.ts
@@ -3,7 +3,7 @@ import { AuditEvent } from './auditEvent'
 export interface S3ObjectDetails {
   auditEvents?: AuditEvent[]
   auditEventsFailedReingest?: AuditEvent[]
-  bucket: string
-  key: string
+  bucket: string | undefined
+  key: string | undefined
   sqsRecordMessageId: string
 }
