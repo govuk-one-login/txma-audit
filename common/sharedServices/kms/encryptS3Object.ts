83cd4ea2c7d5d7d9bd82a0e9ab43567d9e1d11b7
James Holland <james.holland@digital.cabinet-office.gov.uk>
2026-02-03 14:44:10 +0000

    refactored feature flagging

--- common/sharedServices/kms/encryptS3Object.ts
+++ common/sharedServices/kms/encryptS3Object.ts
@@ -6,15 +6,15 @@ import { getEnv } from '../../utils/helpers/getEnv'
 export const encryptS3Object = async (data: Readable): Promise<Buffer> => {
   let keyring: KmsKeyringNode;
 
-  if (process.env.NODE_ENV == 'production') {
-    keyring = new KmsKeyringNode({
-      generatorKeyId: getEnv('GENERATOR_KEY_ID')
-    })
-  } else {
+    if (process.env.BACKUP_ENCRYPTION_ENABLED) {
     keyring = new KmsKeyringNode({
       generatorKeyId: getEnv('GENERATOR_KEY_ID'),
       keyIds: [getEnv('BACKUP_KEY_ID')]
     })
+  } else {
+      keyring = new KmsKeyringNode({
+        generatorKeyId: getEnv('GENERATOR_KEY_ID')
+      })
   }
   // considering including context in encryption - considered good practice to do so
   // if included, the same context object will be required for decryption
